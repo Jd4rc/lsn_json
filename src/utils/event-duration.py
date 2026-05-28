d4ca96ac82a651ceab4cacb7c92249d31498d961
dosed <e.nmitenin@gmail.com>
2026-05-28 14:35:46 +0300

    add reading files

--- src/utils/event-duration.py
+++ src/utils/event-duration.py
@@ -1,2 +1,17 @@
+import json
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent.parent
+
+
 def event_duration(filename: str) -> list[int]:
-    pass
+    file_path = BASE_DIR / 'data' / filename
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    return data
+
+data = event_duration('events.json')
+
+print(data)
