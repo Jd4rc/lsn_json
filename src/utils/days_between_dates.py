924fca79917dff5c81322ec913f7fc8e3d9924f1
dosed <e.nmitenin@gmail.com>
2026-05-29 10:57:55 +0300

    rename var and add docstrirng

--- src/utils/days_between_dates.py
+++ src/utils/days_between_dates.py
@@ -5,11 +5,16 @@ def get_days_between_dates(
         date1: str,
         date2: str
 ) -> int:
-    date1 = datetime.datetime.strptime(date1, '%d.%m.%Y')
-    date2 = datetime.datetime.strptime(date2, '%d.%m.%Y')
+    """
+    Возвращает разницу в днях между двумя датами.
 
-    result = date2 - date1
+    :param date1: Первая дата в формате DD.MM.YYYY.
+    :param date2: Вторая дата в формате DD.MM.YYYY.
+    :return: Разница между датами в днях.
+    """
+    start_date  = datetime.datetime.strptime(date1, '%d.%m.%Y')
+    end_date  = datetime.datetime.strptime(date2, '%d.%m.%Y')
+
+    result = end_date - start_date
 
     return result.days
-
-print(get_days_between_dates("01.01.2022", "31.01.2022"))
