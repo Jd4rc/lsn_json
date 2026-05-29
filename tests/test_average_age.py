5411fa69ef5e74ef7a7e5656b55269bf870dfe6b
egor <e.nmitenin@gmail.com>
2026-05-29 15:41:40 +0300

    add test for invalid data ages

--- tests/test_average_age.py
+++ tests/test_average_age.py
@@ -50,3 +50,28 @@ def test_get_average_age_with_no_data(tmp_path, monkeypatch):
 
     with pytest.raises(ValueError):
         get_average_age('name_and_age.json')
+
+def test_get_average_age_with_invalid_data(tmp_path, monkeypatch):
+    data_dir = tmp_path / "data"
+    data_dir.mkdir()
+
+    file_path = data_dir / 'name_and_age.json'
+
+    test_data = [
+        {'123': '123', '456': '456'},
+        {'Bob': 'Bob', '123': '123', '456': '456'},
+        {'Kate': 123},
+    ]
+
+    file_path.write_text(
+        json.dumps(test_data),
+        encoding="utf-8",
+    )
+
+    monkeypatch.setattr(
+        'src.utils.average_age.BASE_DIR',
+        tmp_path,
+    )
+
+    with pytest.raises(KeyError):
+        get_average_age('name_and_age.json')
