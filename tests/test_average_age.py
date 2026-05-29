476ff627b34f29f8a8d24003d7427b522eda6cf0
egor <e.nmitenin@gmail.com>
2026-05-29 15:52:35 +0300

    Handle invalid type age

--- tests/test_average_age.py
+++ tests/test_average_age.py
@@ -75,3 +75,30 @@ def test_get_average_age_with_invalid_data(tmp_path, monkeypatch):
 
     with pytest.raises(KeyError):
         get_average_age('name_and_age.json')
+
+
+
+def test_get_average_age_with_age_type_str(tmp_path, monkeypatch):
+    data_dir = tmp_path / "data"
+    data_dir.mkdir()
+
+    file_path = data_dir / 'name_and_age.json'
+
+    test_data = [
+        {"name": "Alex", "age": "20"},
+        {"name": "Bob", "age": "25"},
+        {"name": "Kate", "age": "18"}
+    ]
+
+    file_path.write_text(
+        json.dumps(test_data),
+        encoding="utf-8",
+    )
+
+    monkeypatch.setattr(
+        'src.utils.average_age.BASE_DIR',
+        tmp_path,
+    )
+
+    with pytest.raises(TypeError):
+        get_average_age('name_and_age.json')
