3afc1f584cf59292b5903ddbf793a7e387c8a74c
egor <e.nmitenin@gmail.com>
2026-05-29 19:26:36 +0300

    add test for func get adults

---
+++ tests/test_adult_age.py
@@ -0,0 +1,29 @@
+import json
+import pathlib
+import pytest
+from src.utils.adult_age import get_adults
+
+def test_get_adults(tmp_path, monkeypatch):
+    data_path = tmp_path / "data"
+    data_path.mkdir()
+
+    file_path = data_path / 'name_and_age.json'
+
+    test_data = [
+        {"name": "Alex", "age": 20},
+        {"name": "Bob", "age": 25},
+        {"name": "Kate", "age": 18}
+    ]
+
+    file_path.write_text(
+        json.dumps(test_data),
+        encoding="utf-8",
+    )
+
+    monkeypatch.setattr(
+        'src.utils.adult_age.BASE_DIR', tmp_path
+    )
+
+    adults = get_adults('name_and_age.json')
+
+    assert adults == test_data
