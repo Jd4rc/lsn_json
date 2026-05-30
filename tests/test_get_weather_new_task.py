75b8289eba6cff3902f895eaee056dd8105a429c
egor <e.nmitenin@gmail.com>
2026-05-30 22:28:19 +0300

    add test for get_weather

--- tests/test_get_weather_new_task.py
+++ tests/test_get_weather_new_task.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 from unittest.mock import patch
 
-from src.utils.get_weather_new_task import get_coords
+from src.utils.get_weather_new_task import get_coords, get_weather
 
 @patch('src.utils.get_weather_new_task.requests.get')
 def test_get_coords(mock_get, monkeypatch):
@@ -62,3 +62,34 @@ def test_get_coords_without_api_key(monkeypatch):
 
     with pytest.raises(ValueError):
         get_coords('Minsk')
+
+@patch('src.utils.get_weather_new_task.requests.get')
+def test_get_weather(mock_get, monkeypatch):
+    monkeypatch.setattr(
+        'src.utils.get_weather_new_task.API_KEY',
+        'test_api_key'
+    )
+
+    mock_response = mock_get.return_value
+
+    mock_response.json.return_value = {
+        'main': {
+            'temp': 9.83,
+        }
+    }
+
+    assert get_weather(1, 1) == 9.83
+
+    mock_response.raise_for_status.assert_called_once()
+
+    mock_get.assert_called_once_with(
+        'https://api.openweathermap.org/data/2.5/weather',
+        params = {
+        'lat': 1,
+        'lon': 1,
+        'appid': 'test_api_key',
+        'units': 'metric',
+        'lang': 'ru',
+    },
+        timeout=10
+    )
