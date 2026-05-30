e812482d2760e5c9c1a1b4bc6ed5db305ddd0225
egor <e.nmitenin@gmail.com>
2026-05-30 14:08:57 +0300

    add test for func get adults

--- tests/test_adult_age.py
+++ tests/test_adult_age.py
@@ -51,3 +51,29 @@ def test_get_adults_with_invalid_data(tmp_path, monkeypatch):
 
     with pytest.raises(ValueError):
         get_adults('name_and_age.json')
+
+
+
+def test_get_adults_with_another_type_age(tmp_path, monkeypatch):
+    data_path = tmp_path / "data"
+    data_path.mkdir()
+
+    file_path = data_path / 'name_and_age.json'
+
+    test_data = [
+        {"name": "Alex", "age": '20'},
+        {"name": "Bob", "age": '25'},
+        {"name": "Kate", "age": '18'}
+    ]
+
+    file_path.write_text(
+        json.dumps(test_data),
+        encoding="utf-8",
+    )
+
+    monkeypatch.setattr(
+        'src.utils.adult_age.BASE_DIR', tmp_path
+    )
+
+    with pytest.raises(TypeError):
+        get_adults('name_and_age.json')
