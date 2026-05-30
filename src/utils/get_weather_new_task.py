b524a792c2120ec03536469abedf20a164247d87
egor <e.nmitenin@gmail.com>
2026-05-30 16:38:26 +0300

    fix name func and add exception handle

--- src/utils/get_weather_new_task.py
+++ src/utils/get_weather_new_task.py
@@ -10,11 +10,15 @@ API_KEY = os.getenv("API_KEY")
 
 
 
-def get_coord(
+def get_coords(
         city:str
 ) -> tuple[float, float]:
+    if not API_KEY:
+        raise Exception("API_KEY not set")
+
     url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}'
     response = requests.get(url)
+    response.raise_for_status()
 
     data = response.json()
 
