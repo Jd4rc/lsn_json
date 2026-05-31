ac6353db025e0d26a3eec4ea11ce3ea0309872b1
egor <e.nmitenin@gmail.com>
2026-05-31 11:36:59 +0300

    add test for get weather with raise except

--- tests/test_get_weather_new_task.py
+++ tests/test_get_weather_new_task.py
@@ -102,3 +102,19 @@ def test_get_weather_with_missing_api_key(monkeypatch):
 
     with pytest.raises(ValueError, match='API_KEY not set'):
         get_weather(1, 1)
+
+@patch('src.utils.get_weather_new_task.requests.get')
+def test_get_weather_with_request_error(mock_get, monkeypatch):
+    monkeypatch.setattr(
+        'src.utils.get_weather_new_task.API_KEY',
+        'test_api_key'
+    )
+
+    mock_response = mock_get.return_value
+
+    mock_response.raise_for_status.side_effect = (
+        requests.exceptions.HTTPError('404 Not Found')
+    )
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        get_weather(1, 1)
